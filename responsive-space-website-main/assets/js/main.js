dcaa989dd735d2b4ec58fe125680672d93a8d38d
vladahvala <80634246+vladahvala@users.noreply.github.com>
2025-07-16 14:57:40 +0300

    Gsap animation added

--- responsive-space-website-main/assets/js/main.js
+++ responsive-space-website-main/assets/js/main.js
@@ -28,4 +28,11 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== GSAP ANIMATION ===============*/
-
+gsap.from('.home__points', 1.5, {opacity:0, y: -300, delay: .2})
+gsap.from('.home__rocket', 1.5, {opacity:0, y: 300, delay: .3})
+gsap.from('.home__planet-1', 1.5, {opacity:0, x: -200, delay: .8})
+gsap.from('.home__planet-2', 1.5, {opacity:0, x: 200, delay: 1})
+gsap.from('.home__cloud-1', 1.5, {opacity:0, y: 200, delay: 1.2})
+gsap.from('.home__cloud-2', 1.5, {opacity:0, x: 200, delay: 1.3})
+gsap.from('.home__content', 1.5, {opacity:0, y: -100, delay: 1.4})
+gsap.from('.home__title img', 1.5, {opacity:0, x: 100, delay: 1.6})
